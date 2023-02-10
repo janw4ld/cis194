5c0608bb103de36ff66d41f5fbf02e0d41247d2b
janw4ld <janw4ld@tutanota.com>
2023-02-10 11:08:44 +0200

    got rid of Maybe

--- week-2/week-2.hs
+++ week-2/week-2.hs
@@ -15,15 +15,16 @@ fromTile tile = case tile of
   _       -> blank
 
 
-data Coords = C Integer Integer
 
 travEdge :: (Integer -> Picture) -> Picture
 -- This only works because the maze is a square
-travEdge fn = go 10
-  where
-    go :: Integer -> Picture
-    go (-10) = fn (-10)
-    go n     = fn n & go (n-1)
+travEdge fn = go 10 where
+  go :: Integer -> Picture
+  go (-10) = fn (-10)
+  go n     = fn n & go (n-1)
+
+
+data Coords = C Integer Integer
 
 atCoords :: Picture -> Coords -> Picture
 atCoords pic (C x y) = translated (fromIntegral x) (fromIntegral y) pic
@@ -38,58 +39,57 @@ maze :: Coords -> Tile
 maze (C x y)
   | abs x > 4  || abs y > 4  = Blank
   | abs x == 4 || abs y == 4 = Wall
-  | x ==  2 && y <= 0        = Wall
-  | x ==  3 && y <= 0        = Storage
-  | x >= -2 && y == 0        = Box
+  | x ==  2    && y <= 0     = Wall
+  | x ==  3    && y <= 0     = Storage
+  | x >= -2    && y == 0     = Box
   | otherwise                = Ground
 
+
 data Direction = R | U | L | D
+data State = S Direction Coords
 
 initialPose :: State
-initialPose = S (Just D) (C (-3) 3)
-
-data State = S (Maybe Direction) Coords | X Direction Coords
+initialPose = S D (C (-3) 3)
 
 adjacentCoords :: Direction -> Coords -> State
-adjacentCoords direction (C x y) = case direction of
-  R -> X direction (C (x+1)  y)
-  U -> X direction (C  x    (y+1))
-  L -> X direction (C (x-1)  y)
-  D -> X direction (C  x    (y-1))
+adjacentCoords direction (C x y) = S direction newCoords where
+  newCoords = case direction of
+    R -> C (x+1)  y
+    U -> C  x    (y+1)
+    L -> C (x-1)  y
+    D -> C  x    (y-1)
 
 newPose :: Text -> Coords -> State
-newPose key = adjacentCoords map
-  where
-    map = case key of
-      "Right" ->  R
-      "Up"    ->  U
-      "Left"  ->  L
-      "Down"  ->  D
-      -- _      ->  Nothing
+newPose key = adjacentCoords direction where
+  direction = case key of
+    "Right" ->  R
+    "Up"    ->  U
+    "Left"  ->  L
+    "Down"  ->  D
+    -- _      ->  Nothing
 
 
 handleEvent :: Event -> State -> State
-handleEvent (KeyPress key) (S _ c) = wrap c (newPose key c)
-  where
-    -- wrap :: Coords ->(Direction, Coords) -> (Maybe Direction, Coords)
-    wrap c (X d newCoords) = case maze newCoords of
-          Ground  -> S (Just d) newCoords
-          Storage -> S (Just d) newCoords
-          _       -> S Nothing c
+handleEvent (KeyPress "Esc") _ = initialPose
+handleEvent (KeyPress key) (S _ c) = wrap c (newPose key c) where
+  wrap c (S d newCoords) = case maze newCoords of
+    Ground  -> S d newCoords
+    Storage -> S d newCoords
+    _       -> S d c
 handleEvent _ (S d c) = S d c
 
 
 player :: State ->  Picture
 player (S direction c) = atCoords
-  (rotate direction (colored red (styledLettering Bold Monospace ">"))) c & drawMaze
-  where
-    rotate :: Maybe Direction -> Picture -> Picture
+  (rotate direction (colored red (styledLettering Bold Monospace ">"))) c
+  & drawMaze where
+    rotate :: Direction -> Picture -> Picture
     rotate direction pic = case direction of
-      Just U  -> rotated (pi/2) pic
-      Just L  -> rotated pi pic
-      Just D  -> rotated (3*pi/2) pic
-      Just R  -> pic
-      Nothing -> pic
+      U  -> rotated (pi/2) pic
+      L  -> rotated pi pic
+      D  -> rotated (3*pi/2) pic
+      R  -> pic
+      -- Nothing -> pic
 
 
 main :: IO ()
